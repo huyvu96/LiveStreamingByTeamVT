61c6accb2239f5c90e90dc899e77ef9969d74d30
huyvu05051996 <huyvu05051996@gmail.com>
2018-04-11 16:24:55 +0700

    Delete some document

--- ReduxThunk/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import tempReducer from './reducer/tempReducer';
-
-const store = createStore(tempReducer, applyMiddleware(thunk))
-export default store;
--- src/ReduxTest/Redux/action/actionCreator.js
+++
@@ -1,8 +0,0 @@
-import { FILTER_SHOW_ALL,
-    FILTER_MEMORIED,
-    FILTER_NEED_PRACTICE,
-    MEMORIED,
-    ISShow,
-    ADDING,
-    ADDED} from './nameAction';
-
