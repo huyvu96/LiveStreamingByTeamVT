5f0567787928571cf10bf88f9da6576c15603a63
huyvu05051996 <huyvu05051996@gmail.com>
2018-04-11 10:30:36 +0700

    update

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ import App from './App';
 import AppMain from './src/App'
 import User from './src/TabNavigator/User'
 import Notification from './src/TabNavigator/Nofication'
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+  'Warning: componentWillUpdate is deprecated',
+]);
 AppRegistry.registerComponent('LiveStreamingByTeamVT', () => AppMain);
