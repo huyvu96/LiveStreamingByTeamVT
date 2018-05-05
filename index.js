7bd7b23451038d6806d43785072ee1db9cf322aa
huyvu96 <huyvu0505@gmail.com>
2018-05-05 16:18:16 +0700

    updata 5/5

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ YellowBox.ignoreWarnings([
   'Warning: componentWillMount is deprecated',
   'Warning: componentWillReceiveProps is deprecated',
   'Warning: componentWillUpdate is deprecated',
+  'Warning: isMounted(...) is deprecated'
 ]);
-AppRegistry.registerComponent('LiveStreamingByTeamVT', () => SocketDemo);
+AppRegistry.registerComponent('LiveStreamingByTeamVT', () => AppMain);
